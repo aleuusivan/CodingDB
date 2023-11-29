b900e14925b512ed19fe785ad252ddd44a2db3ab
aleuus <34044494+aleuus@users.noreply.github.com>
2023-11-29 00:20:59 -0800

    update from tower (master on USB)
    desktop sync

---
+++ Coding/JS_Practice/cardDeck.js
@@ -0,0 +1,106 @@
+   
+
+////////Deck of Cards///////////////////
+
+
+
+function Cards(face, numValue){
+    this.face = face
+    this.numValue = numValue
+    
+  
+  }
+  
+  
+  //////////Hearts -10-
+  let AceOfHearts = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
+  let TwoOfHearts = new Cards("<img id='Card' src= img/Cards/2.png>", 2)
+  let ThreeOfHearts = new Cards("<img id='Card' src= img/Cards/3.png>", 3)
+  let FourOfHearts = new Cards("<img id='Card' src= img/Cards/4.png>", 4)
+  let FiveOfHearts = new Cards("<img id='Card' src= img/Cards/5.png>", 5)
+  let SixOfHearts = new Cards("<img id='Card' src= img/Cards/6.png>", 6)
+  let SevenOfHearts = new Cards("<img id='Card' src= img/Cards/7.png>=", 7)
+  let EightOfHearts = new Cards("<img id='Card' src= img/Cards/8.png>", 8)
+  let NineOfHearts = new Cards("<img id='Card' src= img/Cards/9.png>", 9)
+  let TenOfHearts = new Cards("<img id='Card' src= img/Cards/10.png>", 10)
+  
+  //////////Spades -10-
+  let AceOfSpades = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
+  let TwoOfSpades = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
+  let ThreeOfSpades = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
+  let FourOfSpades = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
+  let FiveOfSpades = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
+  let SixOfSpades = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
+  let SevenOfSpades = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
+  let EightOfSpades = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
+  let NineOfSpades = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
+  let TenOfSpades = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+  
+  
+  //////////Clubs -10-
+  let AceOfClubs = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
+  let TwoOfClubs = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
+   let ThreeOfClubs = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
+   let FourOfClubs = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
+   let FiveOfClubs = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
+   let SixOfClubs = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
+   let SevenOfClubs = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
+   let EightOfClubs = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
+   let NineOfClubs = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
+   let TenOfClubs = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+  
+  //////////Diamonds -10-
+  let AceOfDiamonds = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
+  let TwoOfDiamonds = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
+  let ThreeOfDiamonds = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
+  let FourOfDiamonds = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
+  let FiveOfDiamonds = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
+  let SixOfDiamonds = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
+  let SevenOfDiamonds = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
+  let EightOfDiamonds = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
+  let NineOfDiamonds = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
+  let TenOfDiamonds = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+  
+  const deckOfCards = [
+    
+  AceOfHearts,
+  TwoOfHearts,
+  ThreeOfHearts,
+  FourOfHearts, 
+  FiveOfHearts, 
+  SixOfHearts, 
+  SevenOfHearts, 
+  EightOfHearts, 
+  NineOfHearts, 
+  TenOfHearts, 
+  AceOfSpades, 
+  TwoOfSpades, 
+  ThreeOfSpades,
+  FourOfSpades, 
+  FiveOfSpades,
+  SixOfSpades,
+  SevenOfSpades,
+  EightOfSpades, 
+  NineOfSpades, 
+  TenOfSpades,
+  AceOfClubs, 
+  TwoOfClubs, 
+  ThreeOfClubs,
+  FourOfClubs, 
+  FiveOfClubs, 
+  SixOfClubs, 
+  SevenOfClubs, 
+  EightOfClubs,
+  NineOfClubs, 
+  TenOfClubs, 
+  TwoOfDiamonds,
+  ThreeOfDiamonds, 
+  FourOfDiamonds,
+  FiveOfDiamonds, 
+  SixOfDiamonds, 
+  SevenOfDiamonds, 
+  EightOfDiamonds, 
+  NineOfDiamonds, 
+  TenOfDiamonds,
+  ]
+  

31542253e53138db8efab48d15a62f14059d355c
aleuus <34044494+aleuus@users.noreply.github.com>
2023-11-29 01:45:48 -0800

    update from tower
    11/29/23 0145

--- Coding/JS_Practice/practiceJS.js
+++ Coding/JS_Practice/practiceJS.js
@@ -13,53 +13,53 @@ function Cards(face, numValue){
 
 
 //////////Hearts -10-
-let AceOfHearts = new Cards(" <img src='img/Cards/11.png'/> ", 11)
-let TwoOfHearts = new Cards("<img id='Card' src= img/Cards/2.png>", 2)
-let ThreeOfHearts = new Cards("<img id='Card' src= img/Cards/3.png>", 3)
-let FourOfHearts = new Cards("<img id='Card' src= img/Cards/4.png>", 4)
-let FiveOfHearts = new Cards("<img id='Card' src= img/Cards/5.png>", 5)
-let SixOfHearts = new Cards("<img id='Card' src= img/Cards/6.png>", 6)
-let SevenOfHearts = new Cards("<img id='Card' src= img/Cards/7.png>=", 7)
-let EightOfHearts = new Cards("<img id='Card' src= img/Cards/8.png>", 8)
-let NineOfHearts = new Cards("<img id='Card' src= img/Cards/9.png>", 9)
-let TenOfHearts = new Cards("<img id='Card' src= img/Cards/10.png>", 10)
+let AceOfHearts = new Cards("img/Cards/11.png", 11)
+let TwoOfHearts = new Cards("img/Cards/2.png", 2)
+let ThreeOfHearts = new Cards("img/Cards/3.png", 3)
+let FourOfHearts = new Cards("img/Cards/4.png", 4)
+let FiveOfHearts = new Cards("img/Cards/5.png", 5)
+let SixOfHearts = new Cards("img/Cards/6.png", 6)
+let SevenOfHearts = new Cards("img/Cards/7.png", 7)
+let EightOfHearts = new Cards("img/Cards/8.png", 8)
+let NineOfHearts = new Cards("img/Cards/9.png", 9)
+let TenOfHearts = new Cards("img/Cards/10.png", 10)
 
 //////////Spades -10-
-let AceOfSpades = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
-let TwoOfSpades = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
-let ThreeOfSpades = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
-let FourOfSpades = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
-let FiveOfSpades = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
-let SixOfSpades = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
-let SevenOfSpades = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
-let EightOfSpades = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
-let NineOfSpades = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
-let TenOfSpades = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+let AceOfSpades = new Cards("img/Cards/11.png", 11)
+let TwoOfSpades = new Cards("img/Cards/2.png", 2)
+let ThreeOfSpades = new Cards("img/Cards/3.png", 3)
+let FourOfSpades = new Cards("img/Cards/4.png", 4)
+let FiveOfSpades = new Cards("img/Cards/5.png", 5)
+let SixOfSpades = new Cards("img/Cards/6.png", 6)
+let SevenOfSpades = new Cards("img/Cards/7.png", 7)
+let EightOfSpades = new Cards("img/Cards/8.png", 8)
+let NineOfSpades = new Cards("img/Cards/9.png", 9)
+let TenOfSpades = new Cards("img/Cards/10.png", 10)
 
 
 //////////Clubs -10-
-let AceOfClubs = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
-let TwoOfClubs = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
- let ThreeOfClubs = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
- let FourOfClubs = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
- let FiveOfClubs = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
- let SixOfClubs = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
- let SevenOfClubs = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
- let EightOfClubs = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
- let NineOfClubs = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
- let TenOfClubs = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+let AceOfClubs = new Cards("img/Cards/11.png", 11)
+let TwoOfClubs = new Cards("img/Cards/2.png", 2)
+let ThreeOfClubs = new Cards("img/Cards/3.png", 3)
+let FourOfClubs = new Cards("img/Cards/4.png", 4)
+let FiveOfClubs = new Cards("img/Cards/5.png", 5)
+let SixOfClubs = new Cards("img/Cards/6.png", 6)
+let SevenOfClubs = new Cards("img/Cards/7.png", 7)
+let EightOfClubs = new Cards("img/Cards/8.png", 8)
+let NineOfClubs = new Cards("img/Cards/9.png", 9)
+let TenOfClubs = new Cards("img/Cards/10.png", 10)
 
 //////////Diamonds -10-
-let AceOfDiamonds = new Cards("<img id='Card' src=img/Cards/11.png >", 11)
-let TwoOfDiamonds = new Cards("<img id='Card' src=img/Cards/2.png >", 2)
-let ThreeOfDiamonds = new Cards("<img id='Card' src=img/Cards/3.png >", 3)
-let FourOfDiamonds = new Cards("<img id='Card' src=img/Cards/4.png >", 4)
-let FiveOfDiamonds = new Cards("<img id='Card' src=img/Cards/5.png >", 5)
-let SixOfDiamonds = new Cards("<img id='Card' src=img/Cards/6.png >", 6)
-let SevenOfDiamonds = new Cards("<img id='Card' src=img/Cards/7.png >", 7)
-let EightOfDiamonds = new Cards("<img id='Card' src=img/Cards/8.png >", 8)
-let NineOfDiamonds = new Cards("<img id='Card' src=img/Cards/9.png >", 9)
-let TenOfDiamonds = new Cards("<img id='Card' src=img/Cards/10.png >", 10)
+let AceOfDiamonds = new Cards("img/Cards/11.png", 11)
+let TwoOfDiamonds = new Cards("img/Cards/2.png", 2)
+let ThreeOfDiamonds = new Cards("img/Cards/3.png", 3)
+let FourOfDiamonds = new Cards("img/Cards/4.png", 4)
+let FiveOfDiamonds = new Cards("img/Cards/5.png", 5)
+let SixOfDiamonds = new Cards("img/Cards/6.png", 6)
+let SevenOfDiamonds = new Cards("img/Cards/7.png", 7)
+let EightOfDiamonds = new Cards("img/Cards/8.png", 8)
+let NineOfDiamonds = new Cards("img/Cards/9.png", 9)
+let TenOfDiamonds = new Cards("img/Cards/10.png", 10)
 
 const deckOfCards = [
   
@@ -133,7 +133,7 @@ TenOfDiamonds,
     stayButton.disabled = true
 
       function startGame() {
-        
+        let total  = 0
         count++;
         if(count== 1){
 
@@ -142,19 +142,27 @@ TenOfDiamonds,
         startButton.style.display = 'none';
 
         // Dealer's First Card://
-
-
         dealerCardOne = Math.floor(Math.random()*52 + 1)
-        document.querySelector('#card7').innerText = (deckOfCards[dealerCardOne].face )
+        var  image1 = document.getElementById('card7');
+        image1.src = deckOfCards[dealerCardOne].face
+
+
+        /* dealerCardOne = Math.floor(Math.random()*52 + 1)
+        document.querySelector('#card7').innerText = (deckOfCards[dealerCardOne].face ) */
 
 
         //////Your first two cards//////////
             const card1 = Math.floor(Math.random()*52 + 1)
-            document.querySelector ('#card1').innerText =(deckOfCards[card1].face)
-            document.querySelector('#score').innerText = ( total+=deckOfCards[card1].numValue )
-
+            var  image2 = document.getElementById('card1');
+            image2.src = deckOfCards[card1].face
             const card2 = Math.floor(Math.random()*52 + 1 )
-            document.querySelector ('#card2').innerText =(deckOfCards[card2].face)
+            var  image3 = document.getElementById('card2');
+            image3.src = deckOfCards[card2].face
+
+            /* document.createElement ('#card1').innerText =(deckOfCards[card1].face) */
+            document.querySelector('#score').innerText = ( total+=deckOfCards[card1].numValue )
+            
+            /* document.querySelector ('#card2').innerText =(deckOfCards[card2].face) */
             document.querySelector('#score').innerText = ( total+=deckOfCards[card2].numValue )
             startButton.classList.add('hidden')
         }
@@ -212,7 +220,7 @@ function hitMe() {
 
        }else if (total < 21){
         document.querySelector('#score').innerText = (total)
-             /* THIS IS THE LINE FOR NATURAL BLACKJACK */
+             ///* THIS IS THE LINE FOR NATURAL BLACKJACK *////
        }else 
        document.querySelector('#score').innerText = ('BlackJack!')
    

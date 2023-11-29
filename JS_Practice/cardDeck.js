4ea63d113d2d1a069103e6f2d2fc9c917d82429e
aleuusivan <aleuus@protonmail.com>
2023-11-28 18:19:59 -0800

    update

---
+++ JS_Practice/cardDeck.js
@@ -0,0 +1,57 @@
+function Cards(face, numValue){
+    this.face = face
+    this.numValue = numValue
+    this.ace = function(){alert ('it's an ace)}
+
+}
+
+
+//////////Hearts -10-
+let AceOfHearts = new Cards("img=", 11)
+let TwoOfHearts = new Cards("img=", 2)
+let ThreeOfHearts = new Cards("img=", 3)
+let FourOfHearts = new Cards("img=", 4)
+let FiveOfFiveOfHearts = new Cards("img=", 5)
+let SixOfHearts = new Cards("img=", 6)
+let SevenOfHearts = new Cards("img=", 7)
+let EightOfHearts = new Cards("img=", 8)
+let NineOfHearts = new Cards("img=", 9)
+let TenOfHearts = new Cards("img=", 10)
+
+//////////Spades -10-
+let AceOfSpades = new Cards("img=", 11)
+let TwoOfSpades = new Cards("img=", 2)
+let ThreeOfSpades = new Cards("img=", 3)
+let FourOfSpades = new Cards("img=", 4)
+let FiveOfFiveOfSpades = new Cards("img=", 5)
+let SixOfSpades = new Cards("img=", 6)
+let SevenOfSpades = new Cards("img=", 7)
+let EightOfSpades = new Cards("img=", 8)
+let NineOfSpades = new Cards("img=", 9)
+let TenOfSpades = new Cards("img=", 10)
+
+
+//////////Clubs -10-
+let AceOfClubs = new Cards("img=", 11)
+let TwoOfClubs = new Cards("img=", 2)
+let ThreeOfClubs = new Cards("img=", 3)
+let FourOfClubs = new Cards("img=", 4)
+let FiveOfFiveOfClubs = new Cards("img=", 5)
+let SixOfClubs = new Cards("img=", 6)
+let SevenOfClubs = new Cards("img=", 7)
+let EightOfClubs = new Cards("img=", 8)
+let NineOfClubs = new Cards("img=", 9)
+let TenOfClubs = new Cards("img=", 10)
+
+//////////Diamonds -10-
+let AceOfDiamonds = new Cards("img=", 11)
+let TwoOfDiamonds = new Cards("img=", 2)
+let ThreeOfDiamonds = new Cards("img=", 3)
+let FourOfDiamonds = new Cards("img=", 4)
+let FiveOfFiveOfDiamonds = new Cards("img=", 5)
+let SixOfDiamonds = new Cards("img=", 6)
+let SevenOfDiamonds = new Cards("img=", 7)
+let EightOfDiamonds = new Cards("img=", 8)
+let NineOfDiamonds = new Cards("img=", 9)
+let TenOfDiamonds = new Cards("img=", 10)
+
